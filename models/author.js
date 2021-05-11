4ca362b2b56ccf216514d7f2b953cff664884ab4
Kn0wn-Un <naikdarsh175@gmail.com>
2021-05-11 16:33:38 +0530

    Initial commit

---
+++ models/author.js
@@ -0,0 +1,50 @@
+var mongoose = require('mongoose');
+const { DateTime } = require('luxon');
+var Schema = mongoose.Schema;
+
+var AuthorSchema = new Schema({
+	first_name: { type: String, required: true, maxLength: 100 },
+	family_name: { type: String, required: true, maxLength: 100 },
+	date_of_birth: { type: Date },
+	date_of_death: { type: Date },
+});
+
+// Virtual for author's full name
+AuthorSchema.virtual('name').get(function () {
+	return this.family_name + ', ' + this.first_name;
+});
+
+// Virtual for author's lifespan
+AuthorSchema.virtual('lifespan').get(function () {
+	return (
+		this.date_of_death.getYear() - this.date_of_birth.getYear()
+	).toString();
+});
+
+// Virtual for author's lifespan
+AuthorSchema.virtual('lifespan_formatted').get(function () {
+	if (!this.date_of_birth) return '';
+	if (!this.date_of_death)
+		return `${DateTime.fromJSDate(this.date_of_birth).toLocaleString(
+			DateTime.DATE_MED
+		)} - `;
+	return `${DateTime.fromJSDate(this.date_of_birth).toLocaleString(
+		DateTime.DATE_MED
+	)} - ${DateTime.fromJSDate(this.date_of_death).toLocaleString(DateTime.DATE_MED)}`;
+});
+
+// Virtual for author's URL
+AuthorSchema.virtual('url').get(function () {
+	return '/catalog/author/' + this._id;
+});
+
+AuthorSchema.virtual('date_of_birth_value').get(function () {
+	return DateTime.fromJSDate(this.date_of_birth).toFormat('yyyy-MM-dd');
+});
+
+AuthorSchema.virtual('date_of_death_value').get(function () {
+	return DateTime.fromJSDate(this.date_of_death).toFormat('yyyy-MM-dd');
+});
+
+//Export model
+module.exports = mongoose.model('Author', AuthorSchema);
